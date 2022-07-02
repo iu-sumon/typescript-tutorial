f67c34538f2b7da2b7c43425d93a0726ee22d695
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-07-02 18:54:10 +0600

    some codes added

---
+++ output/script.js
@@ -0,0 +1,14 @@
+"use strict";
+//exm-1
+let student = "sumon";
+console.log(student);
+//exm-2
+let age;
+age = 9;
+age = 'fahim';
+console.log(age);
+//exm-3
+function add(a, b) {
+    return a * b;
+}
+console.log(add(4, 4));
---
+++ src/script.ts
@@ -0,0 +1,31 @@
+
+//exm-1
+let student="sumon";
+console.log(student);
+
+//exm-2
+let age;
+age=9;
+age='fahim'
+console.log(age);
+
+//exm-3
+function add(a:number,b:number)
+{
+    return a*b;
+}
+console.log(add(4,4));
+
+//exm-4
+const actors = [];
+
+actors.push('papia')
+
+//e-5
+const player={
+    name:"sakib",
+    age:35,
+    iscaptain:true
+}
+player.name='mashrafi'
+
